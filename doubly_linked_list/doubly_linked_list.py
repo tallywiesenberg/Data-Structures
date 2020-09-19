b704cc1ffb78a7fe112a8bb123837981dfd6c158
Tally Wiesenberg <t.j.wies@gmail.com>
2020-09-19 13:59:38 -0400

    Node delete + DLL add_to_head, move_to_front, delete methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,13 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+    
+    def delete():
+        # point neighbor nodes to each other
+        if self.prev:
+            self.next.prev = self.prev
+        if self.next:
+            self.prev.next = self.next
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,9 +34,16 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        node = ListNode(value, prev=None, next=self.head)
-        self.head.prev = node
-        self.head = self.head.prev
+        new_node = ListNode(value)
+        #If DLL is empty, the new node is the head and tail
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        #If DLL has at least one node
+        else:
+            self.head.prev = node
+            self.head = self.head.prev
+        #Update length
         self.length += 1
     """
     Removes the List's current head node, making the
@@ -68,27 +82,24 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
+        #if the node is alrady the head, do nothing
+        if self.head is node:
+            return
         #set a location for iterating through the nodes of the list
         loc = self.head
         #move right through nodes until input node is found in list
         #or until we reach the end of the list
         while loc.right is not None:
             #if we've found the input node in the list
-            if:
-                #break left/right bonds between node and its neighbors
-
+            if loc == node:
                 #connect neighbors of input node to each other (left/right pointers should connect)
-
-                #set input node as left of head
-
-                #set old head as right of input node
-
-                #set input node as new head
-
+                node.delete()
+                #set input node as left of head and set old head as right of input node
+                self.add_to_head(node.value)
                 #return True because node was found in list
             #if we haven't found the node yet, we move right
             else:
-                #moe right
+                #move right
                 loc = loc.right
         #Return false because input node was not found in list
         return False
@@ -97,43 +108,35 @@ class DoublyLinkedList:
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        try:
-            forward = self.head
-            backward = self.tail
-            while forward is not backward or forward.prev is not backward:
-                if forward is not node or backward is not node:
-                    forward = forward.next
-                    backward = backward.prev
-                elif forward is node:
-                    forward.prev.next = forward.next
-                    forward.next.prev = forward.prev
-                else:
-                    backward.prev.next = backward.next
-                    backward.next.prev = backward.prev
-            self.add_to_tail(node)
-        except:
-            return 'Node not found'
+        #set a location for iterating through the nodes from the far left
+
+        #move right until the input node is found in the list 
+        #or until we reach the end of the list
+
+        #if we've found the input node in the list
+
+            #break left/right bonds between input node and its neighbors
+
+            #connect input node's neighbors
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        try:
-            forward = self.head
-            backward = self.tail
-            while forward is not backward or forward.prev is not backward:
-                if forward is not node or backward is not node:
-                    forward = forward.next
-                    backward = backward.prev
-                elif forward is node:
-                    forward.prev.next = forward.next
-                    forward.next.prev = forward.prev
-                else:
-                    backward.prev.next = backward.next
-                    backward.next.prev = backward.prev
-        except:
-            return 'Node not found'
+        #don't need to return value
 
+        #do need to update head, tail
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head: #to get to this if/else statement, the list has to be at least 2 nodes
+            self.head = self.head.next
+            node.next.prev = None
+            node.next = None
+        
+        self.length -= 1
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
